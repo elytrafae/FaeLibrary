4fce1267e8534e13cb4dfd2e3e525e6fa510508b
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-03-14 16:46:33 +0200

    Removed Shimmer Circles

--- API/ShimmerCycle/VanillaShimmerCycles/AncientCobaltBreastplateShimmerCycle.cs
+++
@@ -1,7 +0,0 @@
-﻿using Terraria.ID;
-
-namespace FaeLibrary.API.ShimmerCycle.VanillaShimmerCycles {
-    public class AncientCobaltBreastplateShimmerCycle : ShimmerCycle {
-        internal override int[] BaseItems => [ItemID.AncientCobaltBreastplate, ItemID.JungleShirt];
-    }
-}
--- API/ShimmerCycle/VanillaShimmerCycles/AncientCobaltHelmetShimmerCycle.cs
+++
@@ -1,7 +0,0 @@
-﻿using Terraria.ID;
-
-namespace FaeLibrary.API.ShimmerCycle.VanillaShimmerCycles {
-    public class AncientCobaltHelmetShimmerCycle : ShimmerCycle {
-        internal override int[] BaseItems => [ItemID.AncientCobaltHelmet, ItemID.JungleHat];
-    }
-}

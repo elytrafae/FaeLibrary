36a0bbaa6a84e2e1b818084c4d392c5d49d0a342
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-03-14 16:56:41 +0200

    Added Calamity's InsertShimmerResult method, and removed Calamity compat since it's no longer needed

---
+++ API/FaeUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+
+namespace FaeLibrary.API {
+    public class FaeUtils {
+
+        /// <summary>
+        /// <para>NOTE: This method was simply copy-pasted from Calamity. This is the best way something like this can be achieved, so credit to them.</para>
+        /// Adds a shimmer recipe, while having the result transform into the ingredient's original result.
+        /// <para>This is used for inserting items into various shimmer result trees/loops, like the Class Emblem loop.</para>
+        /// </summary>
+        public static void InsertShimmerResult(int result, int ingredient) {
+            ItemID.Sets.ShimmerTransformToItem[result] = ItemID.Sets.ShimmerTransformToItem[ingredient];
+            ItemID.Sets.ShimmerTransformToItem[ingredient] = result;
+        }
+
+    }
+}

8c0f18408409cefecf5ef8cf08f3627fec6dbdb8
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-08-03 10:53:36 +0300

    QOL for AddBuff on NPCs, release

---
+++ API/ClassExtensions/FaeLibNPCExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace FaeLibrary.API.ClassExtensions {
+    public static class FaeLibNPCExtensions {
+
+        public static void AddBuff<T>(this NPC npc, int duration, bool silent = false) where T : ModBuff => npc.AddBuff(ModContent.BuffType<T>(), duration, silent);
+
+    }
+}
